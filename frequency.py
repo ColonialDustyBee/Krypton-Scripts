755b3fdaf023d3612c25da08026e114bf0a730de
ColonialDustyBee <jv192000@gmail.com>
2025-10-28 19:30:04 -0700

    Writing completed script, pulls correctly

--- frequency.py
+++ frequency.py
@@ -1,8 +1,30 @@
 import sys 
+from collections import Counter
+# Will allow us to open file for reading.
+with open(sys.argv[1], "r") as file:
+    fileContents = file.read()
 
-fileName = sys.argv[1] # gives us the script, run as python3 frequency.py <nameoftextfile>
+# Will return frequency of characters, bigrams and trigrams
+singularChar = Counter(fileContents) 
+bigramChars = Counter(fileContents[i:i + 2] for i in range(len(fileContents) - 1))
+trigramChars = Counter(fileContents[i:i + 3] for i in range(len(fileContents) - 2))
 
-singularChar = {}
-bigramChars = {}
-trigramChars = {}
+
+print("Character Frequencies\n")
+for char, count in singularChar.items():
+    print(f"{char}: {count}")
+
+print("\n Character Bigrams")
+for bg, count in bigramChars.items():
+    print(f"{bg}: {count}")
+
+print("\n Character Trigrams")
+for tg, count in trigramChars.items():
+    print(f"{tg}: {count}")
+
+
+                       
+
+
+
 
